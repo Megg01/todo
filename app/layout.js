15558bf6ec7a85ea2a253d57052162256ac76fd6
Megg01 <mimiigaa3@gmail.com>
2024-08-28 17:36:16 +0800

    init

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({ subsets: ["cyrillic"], variable: "--font-sans" });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
